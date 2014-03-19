07c4f804ddd3082f586baf7a4c35ea292d3ef97a
MollyMoile <marcus.hoile@gmail.com>
2014-03-20 00:15:09 +1100

    skills animate triggered by scroll

--- js/main.js
+++ js/main.js
@@ -13,18 +13,7 @@ $(document).ready(function() {
 
 
 
-// ------- skills animation -----------
 
-var duration = 1200
-	$('.skill').on('click', function(){
-		console.log('clicked');
-		$('.ruby').animate({ width:"80%" }, duration,function(){});
-		$('.rails').animate({ width:"70%" }, duration,function(){});
-		$('.javascript').animate({ width:"60%" }, duration,function(){});
-		$('.css').animate({ width:"40%" }, duration,function(){});
-		$('.jquery').animate({ width:"30%" }, duration,function(){});
-	});
-// ----- end of skills animation ----------
 
 
 
@@ -48,6 +37,21 @@ var duration = 1200
 	});
 // ------> end of sticky nav ------------------
 
+	// --- scroll trigger and skill animation --------
+	$(window).on("scroll", function(){
+	  if($("body").scrollTop() > 2100){
+	  	console.log("scroll triggered")
+	    $(window).off("scroll");
+	    // Do some stuff here ..
+	    // ------- skills animation -----------
+			var duration = 1200		
+			$('.ruby').animate({ width:"80%" }, duration,function(){});
+			$('.rails').animate({ width:"70%" }, duration,function(){});
+			$('.javascript').animate({ width:"60%" }, duration,function(){});
+			$('.css').animate({ width:"40%" }, duration,function(){});
+			$('.jquery').animate({ width:"30%" }, duration,function(){});
+	  }
+	});
 
 });
 
