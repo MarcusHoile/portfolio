1892f8a43e9307d55b640e7f59a3e2a9d65842ce
MollyMoile <marcus.hoile@gmail.com>
2014-03-30 14:20:37 +1100

    javascript code restructure, put interactions into discrete functions

--- js/main.js
+++ js/main.js
@@ -1,8 +1,79 @@
 
+function initialize(){
+	setScrollPins();
+	setJquerySelectors();
+	profilePics();
+
+	$(window).scroll(function() {
+		stickyNav();
+		skillsAnimation();
+		taglineFade();
+	});
 
 
-$(document).ready(function() {
 
+
+
+}
+
+function skillsAnimation(){
+	// ------- skills animation 
+	if ($("body").scrollTop() > 2100) {
+		var duration2 = 1200		
+		$('.ruby').animate({ width:"600px" }, duration2,function(){});
+		$('.rails').animate({ width:"70%" }, duration2,function(){});
+		$('.backbone').animate({ width:"40%" }, duration2,function(){});
+		$('.css').animate({ width:"50%" }, duration2,function(){});
+		$('.jquery').animate({ width:"60%" }, duration2,function(){});
+	}
+} // --------- END skills animation ----------
+
+function taglineFade(){
+	if($("body").scrollTop() > 20) {
+		$('#landing-title-tagline').addClass('animated fadeOutUp');
+	} else if ($("body").scrollTop() < 60) {
+		$('#landing-title-tagline').removeClass('animated fadeOutUp');
+	}
+}
+
+function setScrollPins(){
+	// init controller
+	controller = new ScrollMagic();
+	var scrollDuration = 600;
+	// build scene
+	new ScrollScene({triggerElement: "#trigger1", duration: scrollDuration})
+		.setPin("#about-me")
+		.addTo(controller)
+		.triggerHook(0)
+		.addIndicators();
+	new ScrollScene({triggerElement: "#trigger2", duration: scrollDuration})
+		.setPin("#value-prop")
+		.addTo(controller)
+		.triggerHook(0)
+		.addIndicators();	
+	new ScrollScene({triggerElement: "#trigger3", duration: scrollDuration})
+		.setPin("#projects")
+		.addTo(controller)
+		.triggerHook(0)
+		.addIndicators();			
+	new ScrollScene({triggerElement: "#trigger4", duration: scrollDuration})
+		.setPin("#skills")
+		.addTo(controller)
+		.triggerHook(0)
+		.addIndicators();		
+
+}
+function stickyNav(){
+	var scrollTop = $(window).scrollTop();
+
+	if (scrollTop > stickyNavTop) { 
+		$('nav').addClass('sticky');
+	} else {
+		$('nav').removeClass('sticky'); 
+	}
+};
+
+function setJquerySelectors(){
 	//------- hide/show project titles
 	$('.project-container').on('mouseenter', function(){
 		$(this).find("h2").hide();
@@ -10,32 +81,33 @@ $(document).ready(function() {
 
 	$('.project-container').on('mouseleave', function(){
 		$(this).find("h2").show();
-	});
-	// END HIDE/SHOW project titles --------------
+	}); // END HIDE/SHOW project titles --------------
+
+	var stickyNavTop = $('nav').offset().top;
 
 
 
 	// -------animate scroll on nav clicks
 	$('li a').click(function(){
-	    var $href = $(this).attr('href');
-	    var $anchor = $($href).offset();
-	    $('body').animate({ scrollTop: $anchor.top });
-	    return false;
-	});
-	// ----------- END ANIMATE NAV SCROLL-----------------
+		var $href = $(this).attr('href');
+		var $anchor = $($href).offset();
+		$('body').animate({ scrollTop: $anchor.top });
+		return false;
+	}); // ----------- END ANIMATE NAV SCROLL-----------------
+
+}
+
+function profilePics(){	
 
 	var randNum;
 	var randomProfileImage = randNum;
 	var pics = 2
-
 	// assign numbers to each profile pic
 	var pic1 = 'images/shark_200.jpg'
 	var pic2 = 'images/ashraf_200.jpg'
 	var pic2 = 'images/ashraf_200.jpg'
+
 	// generate random number and closest pic number will be used
-
-
-
 	// profile pic animates on hover
 	$('#profile-pic').on('mouseover', function(){
 		$(this).attr('src', 'images/TechnoTim.gif');
@@ -50,64 +122,9 @@ $(document).ready(function() {
 	})
 	$('#profile-pic').on('mouseleave', function(){
 		$(this).attr('src', randomProfileImage);
-	});
-	// END PROFILE PIC ANIMATION ------------------
+	}); // END PROFILE PIC ANIMATION ------------------	
 
+} // profilePics ---------
 
-	// ---------    sticky nav
-	var stickyNavTop = $('nav').offset().top;
-
-	var stickyNav = function(){
-		var scrollTop = $(window).scrollTop();
-
-		if (scrollTop > stickyNavTop) { 
-			$('nav').addClass('sticky');
-		} else {
-			$('nav').removeClass('sticky'); 
-		}
-	};
-
-	stickyNav();
-
-	$(window).scroll(function() {
-		stickyNav();
-	});
-// ------> END STICKY NAV ------------------
-
-	// --- scroll triggers and animations 
-	$(window).on("scroll", function(){
-		if($("body").scrollTop() > 20) {
-			$('#landing-title-tagline').addClass('animated fadeOutUp');
-		} else if ($("body").scrollTop() < 60) {
-			$('#landing-title-tagline').removeClass('animated fadeOutUp');
-		}
-		//----------- END tagline fade in/out -----------
-
-		// -------- value prop animations 
-		// if($("body").scrollTop() > 1000) {
-		// 	var duration1 = 800;
-		// 	$('.col1').animate({ marginTop: 0 }, duration1, function(){});
-		// 	$('.col2').animate({ marginTop: 200 }, duration1, function(){});
-		// }
-		// ----- END value prop animations -------
-
-		// ------- skills animation 
-	  if($("body").scrollTop() > 2100){
-			var duration2 = 1200		
-			$('.ruby').animate({ width:"600px" }, duration2,function(){});
-			$('.rails').animate({ width:"70%" }, duration2,function(){});
-			$('.backbone').animate({ width:"40%" }, duration2,function(){});
-			$('.css').animate({ width:"50%" }, duration2,function(){});
-			$('.jquery').animate({ width:"60%" }, duration2,function(){});
-	  }
-	  // --------- END skills animation ----------
-	});
-
-	// ------ END SCROLL TRIGGERS ------------
-
-
-	
-
-});
 
 
